33533859362d1fe8562503e1839dbb4f6c7f5be1
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2024-11-06 00:46:20 -0300

    fix: api security based in session and origin

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,43 @@
 import { NextRequest, NextResponse } from 'next/server';
-const cookie = require("cookie");
+import { Outstatic, Session } from "outstatic";
 
-export function middleware(request: NextRequest) {
+// Classe para gerenciar a sessão
+class SessionManager {
+  private session: Session | null = null;
+
+  // Método para carregar e definir a sessão
+  async loadSession() {
+    if (this.session === null) {
+      // Carrega a sessão de forma assíncrona
+      const ostData = await Outstatic();
+      this.session = ostData.session;
+    }
+  }
+
+  // Método para obter a sessão
+  getSession() {
+    return this.session;
+  }
+
+  // Método para definir explicitamente a sessão
+  setSession(session: Session | null) {
+    this.session = session;
+  }
+
+  // Método para verificar se a sessão existe
+  hasSession() {
+    return this.session !== null;
+  }
+}
+
+// Instancia o gerenciador de sessão
+const sessionManager = new SessionManager();
+
+// Middleware
+export async function middleware(request: NextRequest) {
+  // Carrega a sessão (assíncrono)
+  await sessionManager.loadSession();
+
   // Verifica o ambiente (produção ou desenvolvimento)
   const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -13,19 +49,25 @@ export function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  // Recupera o cookie _vercel_jwt
-  const cookies = cookie.parse(request.headers.get('cookie') || '');
-  const vercelJwt = cookies['_vercel_jwt'];
-  
   // Verifica a origem da requisição
   const origin = request.headers.get('Origin');
 
   // Se a origem não for permitida, retorna 403
-  if ((origin && origin !== allowedOrigin) || !vercelJwt) {
+  if (origin && origin !== allowedOrigin) {
     return NextResponse.json({ message: 'Acesso proibido. Origem não permitida.' }, { status: 403 });
   }
 
-  // Se o cookie estiver presente e a origem for permitida, permite a requisição seguir
+  // Condicional para rotas específicas que requerem verificação de sessão
+  const isSessionRequiredRoute = request.url.includes('/api/delete/remove') || request.url.includes('/api/upload');
+
+  if (isSessionRequiredRoute) {
+    // Verifica se a sessão existe
+    if (!sessionManager.hasSession()) {
+      return NextResponse.json({ message: 'Sessão expirada ou não encontrada.' }, { status: 401 });
+    }
+  }
+
+  // Se a origem for permitida e a sessão (se necessário) estiver válida, permite a requisição seguir
   const response = NextResponse.next();
   
   // Adiciona os cabeçalhos CORS para a resposta
