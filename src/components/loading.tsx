f1e2b84d2c4ae984e1856fa30681d404ff02c6c8
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2024-11-16 12:55:41 -0300

    feat: loading screen black opacity

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image';
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-[#00000040] bg-opacity-50 z-50">
-      <div className="flex flex-col items-center justify-center space-y-4">
+    <div className="fixed inset-0 flex items-center justify-center bg-[#00000025] bg-opacity-50 z-50">
+      <div className="flex flex-col items-center justify-center space-y-4 bg-[#ffffff50] rounded-full p-14">
         <div className="animate-pulse">
           <Image
-            src="https://geunaweb.vercel.app/images/logo-geu-transp.png"
+            src="https://www.geuuniao.com.br/images/logo-geu-transp.png"
             alt="Logo Geu"
             width={100}
             height={100}
@@ -14,7 +14,7 @@ export default function Loading() {
             className="max-w-[100px] max-h-[100px]"
           />
         </div>
-        <p className="text-white text-lg">Carregando...</p>
+        <p className="text-sky-600 text-lg">Carregando...</p>
       </div>
     </div>
   );
