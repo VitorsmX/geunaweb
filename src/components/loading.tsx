46311fab2432d6aedeb7200d2a2343300d9ab9b2
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2024-11-16 13:03:17 -0300

    Atualizar o loading.tsx

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-[#00000025] bg-opacity-50 z-50">
-      <div className="flex flex-col items-center justify-center space-y-4 bg-[#ffffff50] rounded-full p-14">
+    <div className="fixed inset-0 flex items-center justify-center bg-[#00000010] bg-opacity-10 z-50">
+      <div className="flex flex-col items-center justify-center space-y-4 bg-[#ffffff50] rounded-full p-[150px]">
         <div className="animate-pulse">
           <Image
             src="https://www.geuuniao.com.br/images/logo-geu-transp.png"
             alt="Logo Geu"
             width={100}
             height={100}
-            quality={50}
+            quality={40}
             className="max-w-[100px] max-h-[100px]"
           />
         </div>
