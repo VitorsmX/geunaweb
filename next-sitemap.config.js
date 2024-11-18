241786ec9cff88543d86ac704d4060a132b64f4b
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2024-11-17 23:13:50 -0300

    add SiteMap

---
+++ next-sitemap.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_APP_URL || 'https://www.geuuniao.com.br',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.geuuniao.com.br/sitemap.xml',
+      'https://www.geuuniao.com.br/sitemap-1.xml',
+      'https://www.geuuniao.com.br/sitemap-2.xml'
+    ],
+    policy: [
+      { userAgent: '*', disallow: ['/outstatic/*'] }
+    ]
+  },
+  changefreq: 'daily',
+  priority: 0.8,
+  sitemapSize: 5000,
+  exclude: ['/outstatic/*'],
+  generateIndexSitemap: false,
+  autoLastmod: true,
+  lastmodDate: true,
+};
