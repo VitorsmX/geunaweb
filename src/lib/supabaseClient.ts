639a39232d5e087c2f463a65df959f8754ec4c9d
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2024-11-20 16:35:02 -0300

    j

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Acessando as variáveis de ambiente para configuração do Supabase
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 
 // Criando uma única instância do cliente Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
