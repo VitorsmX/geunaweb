f55b95c40c1989252d6d0f90a8007f66b3af8059
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2024-11-21 00:13:02 -0300

    djj

--- src/app/api/youtube-upload/route.ts
+++ src/app/api/youtube-upload/route.ts
@@ -1,19 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import supabase from '@/lib/supabaseClient';
 
-// Função para validar URL do YouTube
-const isValidYoutubeUrl = (url: string) => {
-  const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)\/(watch\?v=|shorts\/)([a-zA-Z0-9_-]{11})/;
-  return youtubeRegex.test(url);
-};
-
 export async function POST(req: NextRequest) {
   const { url, slug } = await req.json();
-
-  // Verificar se a URL do YouTube é válida
-  if (!isValidYoutubeUrl(url)) {
-    return NextResponse.json({ error: 'URL do YouTube inválida' }, { status: 400 });
-  }
 
   // Verificar se o slug foi fornecido
   if (!slug) {
