9865991e5604f3a59fdc80122e8ff9f2b862c486
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2025-03-25 12:26:19 -0300

    fix: session context suspense error

--- src/components/SessionContext.tsx
+++ src/components/SessionContext.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { createContext, useContext, useEffect, useState } from "react";
+
+import React, { createContext, useContext, useEffect, useState, Suspense } from "react";
 import Cookies from "js-cookie";
 import Loading from "./loading";
 import { useSearchParams } from "next/navigation";
@@ -7,7 +8,7 @@ import validateEventId from "@/actions/validateEvent";
 
 // Função para gerar um UUID
 function generateUUID() {
-  return crypto.randomUUID(); // Gera um UUID único usando a API nativa do browser
+  return crypto.randomUUID();
 }
 
 // Função para ler o UUID armazenado no cookie
@@ -29,12 +30,16 @@ interface SessionContextType {
 const SessionContext = createContext<SessionContextType | undefined>(undefined);
 
 // Provider que gerencia a sessão e o cookie
-export const SessionProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const SessionProviderInner: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const searchParams = useSearchParams();
-  const paramEventId = searchParams.get("eventid");
-
-  const [uuid, setUuid] = useState<string>("");
+  const [uuid, setUuid] = useState<string | null>(null);
   const [isEventGuest, setIsEventGuest] = useState<boolean>(false);
+  const [paramEventId, setParamEventId] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Obtém o parâmetro da URL dentro do useEffect
+    setParamEventId(searchParams.get("eventid") || null);
+  }, [searchParams]);
 
   useEffect(() => {
     let storedUUID = getUUIDFromCookie();
@@ -48,7 +53,7 @@ export const SessionProvider: React.FC<{ children: React.ReactNode }> = ({ child
   useEffect(() => {
     const checkEventGuest = async () => {
       if (paramEventId) {
-        const isValid = await validateEventId(paramEventId); // Aguarda a resolução da Promise
+        const isValid = await validateEventId(paramEventId);
         setIsEventGuest(isValid);
       }
     };
@@ -66,6 +71,14 @@ export const SessionProvider: React.FC<{ children: React.ReactNode }> = ({ child
   );
 };
 
+// Wrapper para incluir o Suspense necessário
+export const SessionProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <Suspense fallback={<Loading />}>
+      <SessionProviderInner>{children}</SessionProviderInner>
+    </Suspense>
+  );
+};
 
 // Hook para acessar o contexto
 export function useSession() {
