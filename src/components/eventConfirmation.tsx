283205bf06d1bf47462b3bf24659633ac5a4de76
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2025-03-25 14:33:56 -0300

    feat: ui and usability improvements

--- src/components/eventConfirmation.tsx
+++ src/components/eventConfirmation.tsx
@@ -11,7 +11,7 @@ const EventConfirmationBanner = ({ uuid }: { uuid: string }) => {
   const id = isEventGuest ? uuid : false;
 
   // Inicializa o estado do modal como ABERTO se houver um ID válido
-  const [isOpen, setIsOpen] = useState(!!id);
+  const [isOpen, setIsOpen] = useState(true);
   const [fullName, setFullName] = useState("");
   const [whatsapp, setWhatsapp] = useState("");
   const [finalCode, setFinalCode] = useState<string | null>(null);
@@ -21,7 +21,10 @@ const EventConfirmationBanner = ({ uuid }: { uuid: string }) => {
   // Fecha o modal ao clicar fora
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
         setIsOpen(false);
       }
     };
@@ -46,7 +49,7 @@ const EventConfirmationBanner = ({ uuid }: { uuid: string }) => {
           onClick={() => setIsOpen(true)}
           className="fixed bottom-16 right-4 bg-teal-600 text-white py-2 px-4 rounded-full shadow-lg flex items-center space-x-2 border border-green-900 hover:bg-teal-500 transition-all duration-300 ease-in-out z-40"
         >
-          <span className="text-sm w-fit">OBRIGADO POR PARTICIPAR DO EVENTO</span>
+          <span className="text-sm w-fit">GERAR COMPROVANTE (CLIQUE AQUI)</span>
           <Heart />
         </button>
       )}
@@ -54,9 +57,15 @@ const EventConfirmationBanner = ({ uuid }: { uuid: string }) => {
       {/* Modal (já aberto se o ID for válido) */}
       {isOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div ref={modalRef} className="bg-white rounded-lg shadow-xl p-6 max-w-lg mx-4 w-full relative">
+          <div
+            ref={modalRef}
+            className="bg-white rounded-lg shadow-xl p-6 max-w-lg mx-4 w-full relative"
+          >
             {/* Botão para fechar o modal */}
-            <button onClick={() => setIsOpen(false)} className="absolute top-2 right-2 text-gray-600 hover:text-gray-800">
+            <button
+              onClick={() => setIsOpen(false)}
+              className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+            >
               <X size={24} />
             </button>
 
@@ -66,7 +75,9 @@ const EventConfirmationBanner = ({ uuid }: { uuid: string }) => {
               transition={{ duration: 0.8, ease: "easeOut" }}
               className="text-center"
             >
-              <h2 className="text-3xl font-bold uppercase tracking-wide mb-4 text-teal-600">PARABÉNS!</h2>
+              <h2 className="text-3xl font-bold uppercase tracking-wide mb-4 text-teal-600">
+                PARABÉNS!
+              </h2>
               <p className="text-lg leading-relaxed opacity-90 mb-6 text-gray-700">
                 Por participar do NOSSO EVENTO
               </p>
@@ -93,8 +104,11 @@ const EventConfirmationBanner = ({ uuid }: { uuid: string }) => {
 
               {/* Código de participação (só aparece quando os campos são preenchidos) */}
               {finalCode && (
-                <div className="bg-zinc-900 text-white px-4 py-2 rounded-lg text-sm break-all">
-                  <strong>Comprovante:</strong> {finalCode}
+                <div className="flex flex-col gap-y-2">
+                  <div className="bg-zinc-900 text-white px-4 py-2 rounded-lg text-base break-all">
+                    <strong>Comprovante:</strong> {finalCode}
+                  </div>
+                  <p className="hover:scale-110 text-sm font-mono p-2">*Válido somente mediante apresentação do ticket impresso e carimbado</p>
                 </div>
               )}
 
